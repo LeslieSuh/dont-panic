3198e922c2ddf1dd407fba68c2e30bef4f6ad406
Leslie Suh <93383909+LeslieSuh@users.noreply.github.com>
2022-02-09 13:40:39 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const navbar = document.querySelector('.navmenu_drop_gam'),
-  display = document.querySelector('.display_gam'),
-  animation_in = document.querySelector('.animation_in_gam'),
-  animation_out = document.querySelector('.animation_out_gam'),
-  animation_stay = document.querySelector('.animation_stay_gam'),
+const navbar = document.querySelector(".navmenu_drop_gam"),
+  display = document.querySelector(".display_gam"),
+  animation_in = document.querySelector(".animation_in_gam"),
+  animation_out = document.querySelector(".animation_out_gam"),
+  animation_stay = document.querySelector(".animation_stay_gam"),
   navbarHeight = navbar.getBoundingClientRect().height,
-  blog = document.querySelector('.container-yummy'),
-  items1 = document.querySelectorAll('.item1'),
-  items2 = document.querySelectorAll('.item2'),
-  content = document.querySelector('.content_gam'),
-  hamberger = document.querySelector('.menu_icon_gam'),
-  hamberger_fix = document.querySelector('.menu_icon_gam_fix'),
-  fixmenu = document.querySelector('.navbar_menu_gam_fix'),
-  menu = document.querySelector('.navbar_menu_gam');
+  blog = document.querySelector(".container-yummy"),
+  items1 = document.querySelectorAll(".item1"),
+  items2 = document.querySelectorAll(".item2"),
+  content = document.querySelector(".content_gam"),
+  hamberger = document.querySelectorAll(".menu_icon_gam"),
+  hamberger_fix = document.querySelectorAll(".menu_icon_gam_fix"),
+  fixmenu = document.querySelectorAll(".navbar_menu_gam_fix"),
+  menus = document.querySelectorAll(".navbar_menu_gam");
 
 window.onscroll = function () {
   scrollFunction();
@@ -19,52 +19,59 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    navbar.style.top = '0';
+    navbar.style.top = "0";
   } else {
-    navbar.style.top = '-80px';
+    navbar.style.top = "-80px";
   }
 }
 
 const showAnamation = () => {
   setInterval(() => {
-    animation_in.classList.toggle('display_gam');
-    animation_out.classList.toggle('display_gam');
+    animation_in.classList.toggle("display_gam");
+    animation_out.classList.toggle("display_gam");
   }, 7000);
 };
 
 showAnamation();
 
 for (const item1 of items1) {
-  item1.addEventListener('click', () => {
+  item1.addEventListener("click", () => {
     content.scrollIntoView({
-      behavior: 'smooth',
-      block: 'center',
-      inline: 'nearest',
+      behavior: "smooth",
+      block: "center",
+      inline: "nearest",
     });
-
-    menu.classList.remove('menuDrop');
-    fixmenu.classList.remove('menuDrop');
+    menus.forEach((menu) => {
+      menu.classList.toggle("menuDrop");
+    });
   });
 }
-
 for (const item2 of items2) {
-  item2.addEventListener('click', () => {
+  item2.addEventListener("click", () => {
     blog.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'nearest',
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
     });
-
-    menu.classList.remove('menuDrop');
-    fixmenu.classList.remove('menuDrop');
+    menus.forEach((menu) => {
+      menu.classList.toggle("menuDrop");
+    });
   });
 }
 // 모바일 크기 맞췩
 
-hamberger.addEventListener('click', () => {
-  menu.classList.toggle('menuDrop');
+hamberger.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    menus.forEach((menu) => {
+      menu.classList.toggle("menuDrop");
+    });
+  });
 });
 
-hamberger_fix.addEventListener('click', () => {
-  fixmenu.classList.toggle('menuDrop');
+hamberger_fix.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    fixmenu.forEach((menu) => {
+      menu.classList.toggle("menuDrop");
+    });
+  });
 });
